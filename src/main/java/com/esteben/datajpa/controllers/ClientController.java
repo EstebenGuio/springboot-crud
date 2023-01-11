ef2a71dc418f628e44bd1dbe1c6573b9852ab5af
Esteben Guio <Esteben.guiog@gmail.com>
2023-01-10 19:05:34 -0500

    adding flash messages

--- src/main/java/com/esteben/datajpa/controllers/ClientController.java
+++ src/main/java/com/esteben/datajpa/controllers/ClientController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.*;
 
 import javax.validation.*;
 import java.util.Map;
@@ -40,7 +41,7 @@ public class ClientController {
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(@Valid Client c, BindingResult br, Model m, SessionStatus st) {
+    public String save(@Valid Client c, BindingResult br, Model m, RedirectAttributes flash, SessionStatus st) {
         //If cliet object has errors
         if (br.hasErrors()) {
             m.addAttribute("title", "client's form");
@@ -49,14 +50,19 @@ public class ClientController {
 
         clientService.save(c);
         st.setComplete();
-
+        flash.addFlashAttribute("success", "Client created successfully");
         return "redirect:home";
     }
 
     @RequestMapping(value = "/form/{cid}")
-    public String edit(@PathVariable(value = "cid") Long cid, Model m) {
+    public String edit(@PathVariable(value = "cid") Long cid, Model m, RedirectAttributes flash) {
         Client c = null;
+        if (cid == null) {
+            flash.addFlashAttribute("error", "Client with id: " + cid.toString() + "Does not exist");
+            return "redirect:/home";
+        }
         if (cid < 1) {
+            flash.addFlashAttribute("error", "Client id cannot be zero");
             return "redirect:/home";
         }
         c = clientService.findById(cid);
@@ -66,10 +72,11 @@ public class ClientController {
     }
 
     @RequestMapping(value = "/remove/{cid}")
-    public String remove(@PathVariable Long cid) {
+    public String remove(@PathVariable Long cid, RedirectAttributes flash) {
         if (cid > 0) {
             clientService.delete(cid);
         }
+        flash.addFlashAttribute("success", "Client deleted successfully");
         return "redirect:/home";
     }
 }
